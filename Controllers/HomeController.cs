851bcb56673ea075d75d066b359d6b35b7b59d2b
Jesse White <jesse.white6@gmail.com>
2021-06-02 20:37:22 -0700

    create mvc application for new york times api.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MvcApiCall.Models;
+
+namespace MvcApiCall.Controllers
+{
+  public class HomeController : Controller
+  {
+    public IActionResult Index()
+    {
+      var allArticles = Article.GetArticles(EnvironmentVariables.ApiKey);
+      return View(allArticles);
+    }
+  }
+}
+
